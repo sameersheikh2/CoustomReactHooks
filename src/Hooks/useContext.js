15f5d8658860152835a09bc3b845b7d8a13c101a
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-27 19:59:06 +0530

    passed argument and param in useContext to configure different logic

--- src/Hooks/useContext.js
+++ src/Hooks/useContext.js
@@ -1,18 +1,21 @@
-import React,{useState , useEffect} from 'react'
+import {useState , useEffect} from 'react'
 
-const useContext = () => {
+const useContext = (forward = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forward){
+        setCounter((prevCounter) => prevCounter + 1);
+      }
+      else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
-
     return () => clearInterval(interval);
-  }, []);
+  }, [forward]);
   
   return counter;
-
 }
 
 export default useContext
